c09346c48c4c1dfbaa22c89aa026b2cc5baa956e
Jie <1614094+yagay@users.noreply.github.com>
2026-09-13 17:46:18 +0100

    Add lightweight WebView app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.yagay.aihub"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.yagay.aihub"
+        minSdk = 31
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":aihub-core"))
+    implementation(project(":aihub-android"))
+    implementation("androidx.webkit:webkit:1.17.0")
+}
