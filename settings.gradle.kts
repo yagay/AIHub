fccae64747b65a367da7febd478daa298c9ca796
Jie <1614094+yagay@users.noreply.github.com>
2026-09-23 06:04:22 +0100

    Remove browser-core composite build from AIHub

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,27 +2,20 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.mozilla.org/maven2/")
         gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(
+        org.gradle.api.initialization.resolve.RepositoriesMode
+            .FAIL_ON_PROJECT_REPOS
+    )
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.mozilla.org/maven2/")
     }
 }
 
 rootProject.name = "AIHub"
 include(":app")
-
-
-includeBuild("vendor/YBrowser") {
-    dependencySubstitution {
-        substitute(module("com.yagay.ybrowser:browser-core"))
-            .using(project(":browser-core"))
-    }
-}
